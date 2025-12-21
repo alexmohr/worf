5baeff62317dd81008a2219494a4448e38782655
Alexander Mohr <git@mohr.io>
2025-12-21 23:19:44 +0100

    remove documentation link from worf

--- publish-all.sh
+++ publish-all.sh
@@ -12,8 +12,8 @@ cd ..
 for crate in examples/worf-hyprswitch examples/worf-hyprspace examples/worf-warden; do
   echo "Publishing $crate (bin only) ..."
   cd "$crate"
-  cargo publish "$@" 
-  cd - > /dev/null
+  cargo publish "$@"
+  cd - >/dev/null
   sleep 10 # Give crates.io time to update index
   echo
   echo "---"
